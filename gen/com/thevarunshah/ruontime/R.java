d4edbad6cf5b7e828c2dfa80da2d0e505c22b39e
Varun Shah <thevarunshah@gmail.com>
2015-07-31 23:07:56 -0400

    added button icons
    updated buttons on home screen to have icons

--- gen/com/thevarunshah/ruontime/R.java
+++ gen/com/thevarunshah/ruontime/R.java
@@ -22,9 +22,13 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo=0x7f020001;
-        public static final int title=0x7f020002;
+        public static final int ic_directions_black_24dp=0x7f020000;
+        public static final int ic_directions_bus_black_24dp=0x7f020001;
+        public static final int ic_info_black_24dp=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_pin_drop_black_24dp=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int title=0x7f020006;
     }
     public static final class id {
         public static final int about=0x7f080013;
