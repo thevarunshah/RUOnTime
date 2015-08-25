b4eaae531bf452297ce17c5d7413d651e4aa6a88
Varun Shah <thevarunshah@gmail.com>
2015-08-25 01:09:10 -0400

    added thank yous to about screen

--- src/com/thevarunshah/ruontime/AboutScreen.java
+++ src/com/thevarunshah/ruontime/AboutScreen.java
@@ -31,12 +31,15 @@ public class AboutScreen extends Activity {
 		setTitle(Html.fromHtml("<b>"+getTitle()+"</b>"));
 		
 		TextView tv = (TextView) findViewById(R.id.rudots_link);
-	    tv.setText(Html.fromHtml("To view bus route maps and more information about busses, vsit <a href=http://rudots.rutgers.edu/campusbuses.shtml>RU DOTS</a>"));
+	    tv.setText(Html.fromHtml("To view bus route maps and more information about busses, visit <a href=http://rudots.rutgers.edu/campusbuses.shtml>RU DOTS</a>"));
 	    tv.setMovementMethod(LinkMovementMethod.getInstance());
 	    
 	    TextView email = (TextView) findViewById(R.id.email);
 	    email.setText(Html.fromHtml("Bugs/Comments? <a href=\"mailto:varun.shah@rutgers.edu\">Email me!</a>"));
 	    email.setMovementMethod(LinkMovementMethod.getInstance());
+	    
+	    TextView thankYous = (TextView) findViewById(R.id.thankYous);
+	    thankYous.setText(Html.fromHtml("Thank You:<br/>- <b>Alan Chen</b>: logo design<br/>- <b>Pranav Patel</b>: UI/UX Help<br/>- <b>Vaibhav Patel</b>: UI Feedback"));
 	}
 	
 	@Override
